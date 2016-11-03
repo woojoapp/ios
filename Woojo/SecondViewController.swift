194956374d2aece210a30ad5e3721d041e414b7d
Edouard Goossens <egoossens@edouards-macbook-pro.local>
2016-11-03 12:46:12 +0100

    Initial Commit

---
+++ Woojo/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Woojo
+//
+//  Created by Edouard Goossens on 03/11/2016.
+//  Copyright © 2016 Tasty Electrons. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
